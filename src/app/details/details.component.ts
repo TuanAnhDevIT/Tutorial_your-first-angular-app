b603f0133842bf60c23033ce14a2ab54821572df
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-08-29 10:18:35 +0700

    add search functionality

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -17,23 +17,23 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
       details works! {{housingLocationId}}
     </p> -->
     <article>
-    <img class="listing-photo" [src]="housingLocation?.photo"
+      <img class="listing-photo" [src]="housingLocation?.photo"
       alt="Exterior photo of {{housingLocation?.name}}"/>
-    <section class="listing-description">
-      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
-      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
-    </section>
-    <section class="listing-features">
-      <h2 class="section-heading">About this housing location</h2>
-      <ul>
-        <li>Units available: {{housingLocation?.availableUnits}}</li>
-        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
-        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
-      </ul>
-    </section>
-    <section class="listing-apply">
-      <h2 class="section-heading">Apply now to live here</h2>
-      <form [formGroup]="applyForm" (submit)="submitApplication()">
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
         <label for="first-name">First Name</label>
         <input id="first-name" type="text" formControlName="firstName">
 
@@ -44,8 +44,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
         <input id="email" type="email" formControlName="email">
         <button type="submit" class="primary">Apply now</button>
       </form>
-    </section>
-  </article>
+      </section>
+    </article>
   `,
   styleUrls: ['./details.component.css']
 })
--- src/app/housinglocation.ts
+++ src/app/housinglocation.ts
@@ -8,3 +8,4 @@ export interface HousingLocation {
     wifi: boolean;
     laundry: boolean
 }
+
